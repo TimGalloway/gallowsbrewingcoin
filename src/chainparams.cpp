4c674ec6f24f208dd6fcc95d6cf94934165b3c43
Tim Galloway <galloway@iinet.net.au>
2021-03-11 19:35:46 +0800

    Removed dnsseeds etc

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -215,9 +215,9 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
-        vSeeds.emplace_back("testnet-seed.gallowsbrewingcointools.com");
-        vSeeds.emplace_back("seed-b.gallowsbrewingcoin.loshan.co.uk");
-        vSeeds.emplace_back("dnsseed-testnet.thrasher.io");
+        //vSeeds.emplace_back("testnet-seed.gallowsbrewingcointools.com");
+        //vSeeds.emplace_back("seed-b.gallowsbrewingcoin.loshan.co.uk");
+        //vSeeds.emplace_back("dnsseed-testnet.thrasher.io");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,38);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
